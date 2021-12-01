bb4da78124a41f520692973ff462fd0a97bd29f7
yungdeni <asertyx@gmail.com>
2021-12-01 15:09:10 +0100

    Add project files.

---
+++ Day1/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.IO
+
+let pair input = 
+    input |> Array.pairwise
+let findIncreases (a,b) =
+    match a,b with
+    |a,b when a < b -> 1
+    |_ -> 0   
+let threeMeasurement input = 
+    input |> Array.windowed 3 |> Array.map Array.sum |> pair |> Array.map findIncreases |> Array.sum
+
+    
+[<EntryPoint>]
+let main argv =
+    let input = 
+        File.ReadAllLines("../../../input.txt") |> Array.map int 
+    let Part1 = 
+        pair input |> Array.map findIncreases |> Array.sum
+    let Part2 =
+        threeMeasurement input 
+    printfn "Part 1: %i" Part1
+    printfn ""
+    printfn "Part 2: %i" Part2
+    0
